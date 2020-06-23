667ec71a645e887ad2d5b7df20e3ad065ffdf2fd
nikos efthias <nikos@mugsoft.io>
2020-06-24 01:53:21 +0400

    coupon type

---
+++ src/types.js
@@ -0,0 +1,11 @@
+'system' | 'superadmin' | 'admin' | 'seller' | 'user',
+    date_str ?  : string,
+    password ?  : string;
+"SameCardSameRoomMultiBuy" | "SameCardMultiRoomBuy" | "UserBuysFromSingleTable",
+    is_bonus;
+boolean,
+    seller_id;
+String,
+    tables;
+Table[],
+;
